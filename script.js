bbbd33cb53ba9a5088bc351cbe04d9fdbcb561ca
Amirreza Farzan <87432211+jexroid@users.noreply.github.com>
2023-10-09 00:50:37 -0700

    setting animation fixed

--- script.js
+++ script.js
@@ -1,2 +1,253 @@
-let whiteThemed=!0,isPulsing=!1,isTextInputVisible=!0,turnedOn=!1,firstTimeUsingTheApp=!0;document.addEventListener("DOMContentLoaded",function(){//! timer
-let e=document.querySelector(".timer"),t=!1,n;function i(e){return e.toString().padStart(2,"0")}let a=document.querySelector(".overlay"),d=document.getElementById("setone"),l=!1;d.addEventListener("click",function(){!1==l?(a.classList.add("active"),l=!0):!0==l&&(a.classList.remove("active"),l=!1)});let s=document.getElementById("exit-btn"),r=document.getElementById("minimize-btn"),c=document.getElementById("btn1"),o=document.getElementById("vorod"),u=document.getElementById("ip"),v=document.getElementById("port"),m=document.getElementById("username"),$=document.getElementById("password"),p=document.getElementById("warning-msg"),L=document.getElementById("error-msg"),E=document.getElementById("es-circle"),g=document.getElementById("radio1"),y=document.getElementById("radio2"),f=document.getElementById("version"),I=document.getElementById("close-button"),_=document.getElementById("restart-button"),h=document.getElementById("notification"),T=document.querySelector(".image-container"),B=document.getElementById("connect"),k=/^(([01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}([01]?\d{1,2}|2[0-4]\d|25[0-5])$/;s.addEventListener("click",function(){window.WindowInteractApi.close()}),r.addEventListener("click",function(){window.WindowInteractApi.min()}),g.disabled=!0,y.disabled=!0,window.api.LoadCredentials().then(e=>{!1!=e&&(u.value=e.host,v.value=e.port,m.value=e.username,$.value=e.password)}),g.addEventListener("click",()=>{window.api.makeSshTunnel(1)}),y.addEventListener("click",()=>{window.api.makeSshTunnel(0),window.api.openBrowser()}),c.addEventListener("click",function(){window.api.LoadCredentials().then(a=>{if(!1==a)console.log("data is not entered"),T.classList.toggle("notclick"),B.innerHTML="Please Fill the Data",setTimeout(()=>{T.classList.remove("notclick"),B.innerHTML="Connect Now"},3e3);else{if(t)t=!1,e.style.opacity=0,e.style.pointerEvents="none",clearInterval(n);else{let d;d=0,n=setInterval(function(){var t;d++;let n=(t=d,`${i(Math.floor(t/3600))}:${i(Math.floor(t%3600/60))}:${i(t%60)}`);e.textContent=n},1e3),t=!0,e.style.opacity=1,e.style.pointerEvents="auto"}T.classList.toggle("clicked"),!1==turnedOn?(turnedOn=!0,B.innerHTML="Connected!",g.disabled=!1,y.disabled=!1):(turnedOn=!1,B.innerHTML="Connect Now",g.disabled=!0,y.disabled=!0,g.checked=!1,y.checked=!1,window.api.makeSshTunnel(0))}})}),o.addEventListener("click",()=>{switch(!0){case""===u.value||""===v.value||""===m.value||""===$.value:L.classList.add("active"),setTimeout(()=>{L.classList.remove("active")},6e3);break;case!k.test(u.value):E.innerHTML="آیپی وارد شده معتبر نیست",L.classList.add("active"),setTimeout(()=>{L.classList.remove("active")},6e3);break;default:o.innerHTML="اطلاعات ثبت شد",!1===firstTimeUsingTheApp&&p.classList.add("active"),firstTimeUsingTheApp=!1,setTimeout(()=>{o.innerHTML="ورود",p.classList.remove("active")},6e3),window.api.credentials(u.value.trim(),Number(v.value.trim()),m.value.trim(),$.value.trim())}}),u.addEventListener("contextmenu",function(e){e.preventDefault(),navigator.clipboard.readText().then(function(e){u.value=e})}),v.addEventListener("contextmenu",function(e){e.preventDefault(),navigator.clipboard.readText().then(function(e){v.value=e})}),m.addEventListener("contextmenu",function(e){e.preventDefault(),navigator.clipboard.readText().then(function(e){m.value=e})}),$.addEventListener("contextmenu",function(e){e.preventDefault(),navigator.clipboard.readText().then(function(e){$.value=e})});(async()=>await window.WindowInteractApi.ver())().then(e=>{f.innerHTML="v"+e}),I.addEventListener("click",()=>{h.classList.add("hidden")}),_.addEventListener("click",()=>{window.WindowInteractApi.restart()}),f.addEventListener("click",()=>{window.WindowInteractApi.openURLInBrowser("https://github.com/jexroid/group")})});
+// * SSH CONNECTION FUNCTION
+let whiteThemed = true;
+let isPulsing = false;
+let isTextInputVisible = true;
+let turnedOn = false;
+let toggleOverlay = false;
+let isWindowsRegistered = async () => {
+  return await window.api.makeSshTunnel(1);
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  //! timer
+  const timer = document.querySelector(".timer");
+  let isClicked = false;
+  let intervalId;
+
+
+  function startTimer() {
+    let seconds = 0;
+    intervalId = setInterval(function () {
+      seconds++;
+      const formattedTime = formatTime(seconds);
+      timer.textContent = formattedTime;
+    }, 1000);
+  }
+
+  function formatTime(seconds) {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(remainingSeconds)}`;
+  }
+
+  function padZero(number) {
+    return number.toString().padStart(2, "0");
+  }
+
+  // code for overlay of the setting:
+  const second_setting = document.getElementById("setone");
+  const overlay = document.querySelector(".overlay");
+
+  let SettingClicked = false;
+  second_setting.addEventListener("click", function () {
+    if (SettingClicked == false) {
+      overlay.classList.add("active");
+      overlay.classList.remove("deactive"); 
+      SettingClicked = true;
+    } else if (SettingClicked == true) {
+      overlay.classList.remove("active");
+      overlay.classList.add("deactive");
+      SettingClicked = false;
+    }
+  });
+
+  // ? JS STYLE FUNCTIONS
+  const exitBtn = document.getElementById("exit-btn");
+  const minimizeBtn = document.getElementById("minimize-btn");
+  const start = document.getElementById("btn1");
+  const validation = document.getElementById("vorod");
+  const ip = document.getElementById("ip");
+  const port = document.getElementById("port");
+  const username = document.getElementById("username");
+  const password = document.getElementById("password");
+  const warn = document.getElementById("warning-msg");
+  const errorBox = document.getElementById("error-msg");
+  const errorMsg = document.getElementById("es-circle");
+  const radio1 = document.getElementById("radio1");
+  const radio2 = document.getElementById("radio2");
+  const version = document.getElementById("version");
+  const ClosinUpdate = document.getElementById("close-button");
+  const RestartUpdate = document.getElementById("restart-button");
+  const notification = document.getElementById("notification");
+  const imageContainer = document.querySelector(".image-container");
+  const coonectMSG = document.getElementById("connect");
+
+  const ipRegex =
+    /^(([01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}([01]?\d{1,2}|2[0-4]\d|25[0-5])$/;
+  // ! NAVIGATION BTN
+
+  exitBtn.addEventListener("click", function () {
+    window.WindowInteractApi.close();
+  });
+
+  minimizeBtn.addEventListener("click", function () {
+    window.WindowInteractApi.min();
+  });
+
+  // ! NAVIGATION BTN
+  radio1.disabled = true;
+  radio2.disabled = true;
+  //* loading saved data (if exists)
+  window.api.LoadCredentials().then((data) => {
+    if (data != false) {
+      ip.value = data.host;
+      port.value = data.port;
+      username.value = data.username;
+      password.value = data.password;
+    }
+  });
+
+  //* loading saved data (if exists)
+
+  // ? BROWSER BTN
+  // browserBtn.addEventListener("click", function () {
+  //   window.api.openBrowser();
+  // });
+  radio1.addEventListener("click", () => {
+    window.api.makeSshTunnel(1);
+  });
+  radio2.addEventListener("click", () => {
+    window.api.makeSshTunnel(0);
+    // opening browser
+
+    window.api.openBrowser();
+  });
+
+  start.addEventListener("click", function () {
+    window.api.LoadCredentials().then((data) => {
+      if (data == false) {
+        console.log("data is not entered");
+        imageContainer.classList.toggle("notclick");
+        coonectMSG.innerHTML = "Please Fill the Data"
+        setTimeout(() => {
+          imageContainer.classList.remove("notclick");
+          coonectMSG.innerHTML = "Connect Now"
+        }, 3000)
+      } else {
+        if (!isClicked) {
+          isClicked = true;
+          timer.style.opacity = 1;
+          timer.style.pointerEvents = "auto";
+        } else {
+          isClicked = false;
+          timer.style.opacity = 0;
+          timer.style.pointerEvents = "none";
+          clearInterval(intervalId);
+        }
+        imageContainer.classList.toggle("clicked");
+        if (turnedOn == false) {
+          turnedOn = true;
+          coonectMSG.innerHTML = "Connected!";
+          startTimer()
+          radio1.disabled = false;
+          radio2.disabled = false;
+        } else {
+          turnedOn = false;
+          coonectMSG.innerHTML = "Connect Now";
+          radio1.disabled = true;
+          radio2.disabled = true;
+          formatTime()
+
+          // switching the radio bottoms if one of them were on
+          radio1.checked = false;
+          radio2.checked = false;
+          window.api.makeSshTunnel(0);
+        }
+      }
+    })
+    
+  });
+
+  //? validating user data
+  validation.addEventListener("click", () => {
+    switch (true) {
+      case ip.value === "" ||
+        port.value === "" ||
+        username.value === "" ||
+        password.value === "":
+        errorBox.classList.add("active");
+        setTimeout(() => {
+          errorBox.classList.remove("active");
+        }, 6000);
+        break;
+      case !ipRegex.test(ip.value):
+        errorMsg.innerHTML = "آیپی وارد شده معتبر نیست";
+        errorBox.classList.add("active");
+        setTimeout(() => {
+          errorBox.classList.remove("active");
+        }, 6000);
+        break;
+      default:
+        validation.innerHTML = "اطلاعات ثبت شد";
+        warn.classList.add("active");
+        setTimeout(() => {
+          validation.innerHTML = "ورود";
+          warn.classList.remove("active");
+        }, 6000);
+        window.api.credentials(
+          ip.value.trim(),
+          Number(port.value.trim()),
+          username.value.trim(),
+          password.value.trim()
+        );
+        break;
+    }
+  });
+  //? validating user data
+
+  //* copy paste using click right
+  ip.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+
+    navigator.clipboard.readText().then(function (copiedText) {
+      ip.value = copiedText;
+    });
+  });
+
+  port.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+
+    navigator.clipboard.readText().then(function (copiedText) {
+      port.value = copiedText;
+    });
+  });
+
+  username.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+
+    navigator.clipboard.readText().then(function (copiedText) {
+      username.value = copiedText;
+    });
+  });
+
+  password.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+
+    navigator.clipboard.readText().then(function (copiedText) {
+      password.value = copiedText;
+    });
+  });
+  //* copy paste using click right
+
+  // ? VERSION
+  let VERSION = async () => {
+    return await window.WindowInteractApi.ver();
+  };
+  VERSION().then((value) => {
+    version.innerHTML = "v" + value;
+  });
+
+  // * UPDATING THE APP
+  ClosinUpdate.addEventListener("click", () => {
+    notification.classList.add("hidden");
+  })
+
+  RestartUpdate.addEventListener("click", () => {
+    window.WindowInteractApi.restart();
+  })
+
+  version.addEventListener("click", () => {
+    window.WindowInteractApi.openURLInBrowser('https://github.com/jexroid/group')
+  })
+});
